70bab8a14c988b9ef9b15b038f5c395f8329f175
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-07-26 23:03:15 -0400

    initial draft changes to script.js, most character arrays defined but no pw construction yet

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 // Assignment code here
 
+// Global variables
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let alphabetUpper = [...alphabet];
+let alphabetLower = [...alphabet.toLowerCase()];
+let numerals = "0123456789";
+let numeralsArray = [...numerals];
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+// Generate random password after prompting user for criteria
+function generatePassword() {
+  let useUpper    = confirm("Use UPPER CASE letters?");
+  let useLower    = confirm("Use lower case letters?");
+  let useNumerals = confirm("Use decimal numerals");
+  if (!(useUpper || useLower || userNumerals)) {
+    // TODO - also alert, or just let calling function handle?
+    return null;  
+  }
+  let charsForPassword = [];
+  if (useUpper) {
+    charsForPassword = [...charsForPassword, ...alphabetUpper];
+  }
+  if (useLower) {
+    charsForPassword = [...charsForPassword, ...alphabetLower];
+  }
+  if (useNumerals) {
+    charsForPassword = [...charsForPassword, ...numeralsArray];
+  }
+  // TODO - must also accommodate special characters
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  // TODO - can we test for null here?
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
