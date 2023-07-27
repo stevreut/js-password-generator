164f4032e70d9c5d44e3179e2b972eb2f4f1af7f
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-07-27 15:01:25 -0400

    Finish documentation, use constants for min/max password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 // Global variables
 
-let charsForPassword = [];
+const MIN_LEN = 8;    // minimum allowed length of password
+const MAX_LEN = 128;  // maximum allowed length of password
+let charsForPassword = [];  // array of all candidate characters to be chosen from when generating password
 let alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numerals = "0123456789";
-// Note:  These are the special characters as provided by OWASP
-// ( https://owasp.org/www-community/password-special-characters ) with
-// certain caveats:
+let specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";  // from OWASP 
+// ( https://owasp.org/www-community/password-special-characters )
+// Some caveats re the specialChars variable above:
 //    1. The space character has been eliminated from the list.  No good can come from
 //         having a space in a password.
 //    2. The quotation mark indicated as a special character had to be "escaped" in the
@@ -13,16 +15,19 @@ let numerals = "0123456789";
 //    3. The backslash indicated as a special character had to be "escaped" in the
 //         string below by preceding with it with another backslash.  Thus, the \\
 //         below is for ONE backslash.
-let specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 
-// Get references to the #generate element
+// Get references to the #generate element, which is the "Generate Password" button.
 let generateBtn = document.querySelector("#generate");
 
 
 // Prompts for whether a given set of characters (as a string) is to be used and,
 // if accepted by user, add to charsForPassword
 function promptAndAddSubstring(question, substr) {
+  // Parameters:
+  //   question - verbiage displayed at prompt() call
+  //   substr - string to be converted to array of characters if used
+  //   returns:  boolean - true iff user opts to use substr based on response to prompt()
   // TODO - confirm() prompts show buttons "cancel" and "OK".  These
   // labels are not ideal for this app.  Can these be changed, or is
   // there an alternative to confirm that serves our purposes?
@@ -35,89 +40,95 @@ function promptAndAddSubstring(question, substr) {
 }
 
 
+// Prompts the user for a desired password length and validates that the provided result 
+// is both numeric and within the required range - no less than MIN_LEN nor more than MAX_LEN.
+// Allows up to three total tries.
 function promptForLength() {
   let retryCount = 0;
-  let validCount = false;
-  let length = 0;
+  let isValidCount = false;
+  let length = 0;  // length to be returned, provided a valid length is provided
   let countAsString;
-  while (!validCount && retryCount < 3) {
-    validCount = false;
+  while (!isValidCount && retryCount < 3) {
+    isValidCount = false;
     if (retryCount === 0) {
       countAsString = prompt("How many characters in password?");
     } else {
       countAsString= prompt("Invalid response. Try again. How many characters in password?");
     }
+    // Attempt conversion of string value to numeric value
     length = parseInt(countAsString);
     if (length !== null) {
-      if (length >= 8 && length <= 128) {
-        validCount = true;
+      // null return from parseInt indicates failed conversion, so non-null
+      // means we have a numeric value to validate
+      if (length >= MIN_LEN && length <= MAX_LEN) {
+        // Response is only valid if BOTH numeric AND in-range
+        isValidCount = true;
       }
     }
     retryCount++;
   }
-  if (validCount) {
+  if (isValidCount) {
     return length;
   } else {
-    return null;
+    return null;  // signals caller that no valid response was provided
   }
 }
 
-
+// Hides the alert message (a sometimes visible and sometimes hidden paragraph).  Used to reset the
+// state of the screen upon starting a new user dialog. 
 function hideUserAlert() {
-  // TODO - not yet implemented
   // As initial state before processing, use script to hide alert box
   // and blank out content.
-  console.log("alert box reset");
   let alertParagraph = document.getElementById("alert-msg");
-  if (alertParagraph === null) {
-    return;  // TODO
-  }
-  alertParagraph.textContent = "X";  // TODO
+  // Single place holder character (never visible) keeps the page elements from migration, which
+  // the sometimes seem to do if alertParagraph.textContent is valued as "".
+  alertParagraph.textContent = "X";
   alertParagraph.style.visibility = "hidden";
-  // TODO - note: This appears to work, but its effect is delayed until
-  // after all user selections. (?)
+  // Unfortunately, the change of visibility upon hiding herein does not get rendered on the page
+  // until all user responses are entered despited this function being executed immediately upon
+  // pressing the button.  This appears to be a known side effect of the use of the prompt() function.
 }
 
 
 function alertUser(message) {
-  // TODO - net yet implemented
-  console.log("messsage would be '" + message + "'");
-  // TODO - ultimately, unhide box on screen (CSS) and place message there.
   let alertParagraph = document.getElementById("alert-msg");
-  if (alertParagraph === null) {
-    return;  // TODO
-  }
   alertParagraph.textContent = message;
   alertParagraph.style.visibility = "visible";
 }
 
 
-// Generate random password after prompting user for criteria
+// Generate random password after prompting user for criteria or, if user responses are invalid,
+// sets password to empty string and sets an alert message on the page.
 function generatePassword() {
+  // Reset alert message on page to hidden/blank
   hideUserAlert();
+  // Array to hold all characters from which random selections are to be made
+  // when building password (pw), first initialized as empty array.
   charsForPassword = [];
-  hideUserAlert();
+  // hideUserAlert();  TODO - remove this line eventually
+  // Build charsForPassword (the array of candidate characters) based on user responses for
+  // each of the four categories of characters.
   let useUpper = promptAndAddSubstring ("Use UPPER CASE letters?", alphabetUpper);
   let useLower = promptAndAddSubstring ("Use lower case letters?", alphabetUpper.toLowerCase());
   let useNumerals = promptAndAddSubstring ("User decimal numerals?", numerals);
   let useSpecial = promptAndAddSubstring ("Use special characters?", specialChars);
+  // If NO selections are made then we cannot generate a password
   if (!(useUpper || useLower || useNumerals || useSpecial)) {
     alertUser("No character types were selected");
     return "";
   }
-  // // TODO - TEMPORARY code to test correct populate of character arrays:
-  // let len = charsForPassword.length;
-  // console.log("len = " + len);
-  // for (let i=0; i<len; i++) {
-  //   console.log ("char " + i + " of " + len + " : " + charsForPassword[i]);
-  // }
-  // // TODO - TEMPORARY code end
+  // Get the desired password length and validate.  Null is returned from
+  // promptForLength() of no valid length was entered after multiple retries.
   let desiredCharCount = promptForLength();
   if (desiredCharCount === null) {
-    alertUser("No valid length was selected.");
+    alertUser("No valid length was selected.  Length must be between " +
+      MIN_LEN + " and " + MAX_LEN + " inclusive.");
     return "";
   }
   let pw = "";  // password to be returned - initially set as empty string
+  // Select desiredCharCount random characters from array charsForPassword and
+  // append each to pw, thus creating a string of random characters of the
+  // desired length.
   for (let i=0; i<desiredCharCount; i++) {
     let charIndex = Math.floor(Math.random()*charsForPassword.length);
     pw += charsForPassword[charIndex];
@@ -126,13 +137,14 @@ function generatePassword() {
 }
 
 
-// Write password to the #password input
+// Write password to the #password text area on page if possible, or
+// write an alert to on the page.
 function writePassword() {
-  let password = generatePassword();
+  let password = generatePassword();  // Note that generatePassword() returns an empty string when an error occurs.
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 
-// Add event listener to generate button
+// Add event listener to Generate Password button
 generateBtn.addEventListener("click", writePassword);
